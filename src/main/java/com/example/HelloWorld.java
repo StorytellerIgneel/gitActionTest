c3cb07c80490379a8d17855d368a41f6b6b5fd9c
StorytellerIgneel <107390793+StorytellerIgneel@users.noreply.github.com>
2025-04-22 16:01:26 +0800

    Trigger Jenkins Pipeline

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -3,6 +3,7 @@ package com.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.err.println("Jenkins Test Prac10");
     }
 
     public String getGreeting() {
